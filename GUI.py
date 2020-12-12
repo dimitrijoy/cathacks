06602845c6f2f5ec2b1c36c3302c1d2350c2cdf9
Dimitri <dimitri6a6f79@gmail.com>
2020-12-11 23:50:27 -0600

    Cleaned up gui.py in accordance with python styling

--- GUI.py
+++ GUI.py
@@ -1,93 +1,77 @@
-import pygame
+# dependencies
 from game import Board
-pygame.init()
+import pygame
 
-# Window Caption
-Caption = "Chess"
-pygame.display.set_caption(Caption)
+pygame.init() # initializes the game
 
-# Window Dimensions
-size = width, height = ((800,800))
-screenPieces = screen = pygame.display.set_mode(size)
+# window caption
+CAPTION = "Chess"
+pygame.display.set_caption(CAPTION)
 
-# Colors
-white = (192, 192, 192)
-black = (40, 40, 40)
+# window dimens
+DIMENS = WIDTH, HEIGHT = ((800,800))
+screenPieces = screen = pygame.display.set_mode(DIMENS)
 
-# Displays the game board
-def createBoard():
-    for i in range(8):
-            for j in range(8):
-                pygame.draw.rect(screen, white, [(i*2)*100, (2*j)*100, 100, 100])
-                pygame.draw.rect(screen, black, [(2*i-1)*100, (2*j)*100, 100, 100])
-                #pygame.draw.rect(screenPieces, white, [(i*2)*100, (2*j)*100, 100, 100])
-                #pygame.draw.rect(screenPieces, black, [(2*i-1)*100, (2*j)*100, 100, 100])
+# colors
+WHITE = (192, 192, 192)
+BLACK = (40, 40, 40)
 
-                pygame.draw.rect(screen, white, [(2*i-1)*100, (2*j-1)*100, 100, 100])
-                pygame.draw.rect(screen, black, [(2*i)*100, (2*j-1)*100, 100, 100])
-                #pygame.draw.rect(screenPieces, white, [(2*i-1)*100, (2*j-1)*100, 100, 100])
-                #pygame.draw.rect(screenPieces, black, [(2*i)*100, (2*j-1)*100, 100, 100])
-
-# Game Pieces
-blackBishop = pygame.image.load("images/blackBishopElf.png")
-blackPawn = pygame.image.load("images/blackPawnElf.png")
-blackRook = pygame.image.load("images/blackRookSanta.png")
-blackKnight = pygame.image.load("images/blackKnightReindeer.png")
-blackQueen = pygame.image.load("images/blackQueenSanta.png")
-blackKing = pygame.image.load("images/blackKingSanta.png")
-whiteBishop = pygame.image.load("images/whiteBishopElf.png")
-whitePawn = pygame.image.load("images/whitePawnElf.png")
-whiteRook = pygame.image.load("images/whiteRookSanta.png")
-whiteKnight = pygame.image.load("images/whiteKnightReindeer.png")
-whiteQueen = pygame.image.load("images/whiteQueenSanta.png")
-whiteKing = pygame.image.load("images/whiteKingSanta.png")
-empty = pygame.image.load("images/blank.png")
-
-# Organizing for quick allocation
-pieces = {'p':blackPawn, 'P':whitePawn, 'r':blackRook, 'R':whiteRook, 'k':blackKnight, 'K':whiteKnight,
-          'b':blackBishop, 'B':whiteBishop, 'q':blackQueen, 'Q':whiteQueen, 'a':blackKing, 'A':whiteKing,
-          ' ':empty}
-
-
-def start():
-    for i in range(8):
-            for j in range(8):
-                screenPieces.blit(pieces[board.at(i,j)], ((j*100)+25, (i*100)+25))
-
-def moveUpdate():
-    createBoard()
-    for i in range(8):
-            for j in range(8):
-                screenPieces.blit(pieces[board.at(i,j)], ((j*100)+25, (i*100)+25))
-                
-
-
-
-
-done = False
+# game attrs
 board = Board()
-createBoard()
-start()
+b = pygame.image.load("images/blackBishopElf.png")
+p = pygame.image.load("images/blackPawnElf.png")
+r = pygame.image.load("images/blackRookSanta.png")
+k = pygame.image.load("images/blackKnightReindeer.png")
+q = pygame.image.load("images/blackQueenSanta.png")
+a = pygame.image.load("images/blackKingSanta.png")
+B = pygame.image.load("images/whiteBishopElf.png")
+P = pygame.image.load("images/whitePawnElf.png")
+R = pygame.image.load("images/whiteRookSanta.png")
+K = pygame.image.load("images/whiteKnightReindeer.png")
+Q = pygame.image.load("images/whiteQueenSanta.png")
+A = pygame.image.load("images/whiteKingSanta.png")
+FREE = pygame.image.load("images/blank.png")
 
-#Running
-while not done :
+# pieces to corresponding images
+pieces = {'p': p, 'P': P, 'r': r, 'R': R, 'k': k, 'K': K,
+          'b': b, 'B': B, 'q': q, 'Q': Q, 'a': a, 'A': A,
+          ' ': FREE}
+
+# displays the game board
+def create_board():
+    for i in range(8):
+        for j in range(8):
+            pygame.draw.rect(screen, WHITE, [(i*2)*100, (2*j)*100, 100, 100])
+            pygame.draw.rect(screen, BLACK, [(2*i-1)*100, (2*j)*100, 100, 100])
+            pygame.draw.rect(screenPieces, WHITE, [(i*2)*100, (2*j)*100, 100, 100])
+            pygame.draw.rect(screenPieces, BLACK, [(2*i-1)*100, (2*j)*100, 100, 100])
+            pygame.draw.rect(screen, WHITE, [(2*i-1)*100, (2*j-1)*100, 100, 100])
+            pygame.draw.rect(screen, BLACK, [(2*i)*100, (2*j-1)*100, 100, 100])
+            pygame.draw.rect(screenPieces, WHITE, [(2*i-1)*100, (2*j-1)*100, 100, 100])
+            pygame.draw.rect(screenPieces, BLACK, [(2*i)*100, (2*j-1)*100, 100, 100])
+
+# updates board after moves
+def update_board():
+    create_board()
+    for i in range(board.DIMENS):
+            for j in range(board.DIMENS):
+                screenPieces.blit(pieces[board.at(i,j)], ((j*100)+25, (i*100)+25))    
+
+update_board() # init
+while True:
     for event in pygame.event.get():
-        # Closes program
-        if event.type == pygame.QUIT:
-            done = True
-        #Takes coordinates once the mouse is pressed down
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            cordinatesInit = pygame.mouse.get_pos()
-            xInitial = cordinatesInit[1] // 100
-            yInitial = cordinatesInit[0] // 100
-        #Takes coordinates once the mouse is lifted up, then moves the piece and updates the board
-        if event.type == pygame.MOUSEBUTTONUP:
-            cordinatesFin = pygame.mouse.get_pos()
-            xFinal = cordinatesFin[1] // 100
-            yFinal = cordinatesFin[0] // 100
-            board.move((xInitial,yInitial),(xFinal,yFinal))
-            moveUpdate()
-
-            
-        #Displays GUI
+        if event.type == pygame.QUIT: # exits game
+            exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN: # gets cursor coordinates on mouse button down to select piece
+            coordinates_init = pygame.mouse.get_pos()
+            x_init = coordinates_init[1] // 100
+            y_init = coordinates_init[0] // 100
+        elif event.type == pygame.MOUSEBUTTONUP: # gets cursor coordinates on mouse button up to drop [move] piece
+            coordinates_fin = pygame.mouse.get_pos()
+            x_fin = coordinates_fin[1] // 100
+            y_fin = coordinates_fin[0] // 100
+            board.move((x_init, y_init), (x_fin, y_fin))
+            update_board()
+    
+        # displays GUI
         pygame.display.flip()
